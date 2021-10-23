e3b6e24810ea0f99821f45e9d9b66c6fbbc958fd
Micaela <92948781+micamartinez@users.noreply.github.com>
2021-10-23 18:10:16 -0300

    Create Exercise-16.py

---
+++ Exercises/Guía1-Basics/Exercise-16.py
@@ -0,0 +1,7 @@
+'''Create a program that asks the user their age and shows by
+screen all the years he has turned (from 1 to current age).'''
+
+age = int(input('How old are you? '))
+
+for i in range(1, age + 1):
+	print(i)
