501b54adfd842b248fa7ef72337d1180eabfe97c
Micaela <92948781+micamartinez@users.noreply.github.com>
2021-10-22 22:53:26 -0300

    Create Exercise-9.py

---
+++ Exercises/Guía1-Basics/Exercise-9.py
@@ -0,0 +1,19 @@
+'''A toy store usually do mail order sales and the logistics company charges them by weight
+of each package so they must calculate the weight of the clowns and dolls that will come out
+in each package on demand. Each clown weighs 112g and each doll 75g. Create a
+program that reads the number of clowns and dolls sold in the last order and
+calculate the total weight of the package to be shipped.'''
+
+
+number_c = int(input('Number of clowns sold: '))
+number_d = int(input('Number of dolls sold: '))
+
+clown = 112 #grams
+doll = 75 #grams
+package = number_c * clown + number_d * doll
+
+if package < 1000:
+    print(f'The total weight of the package is {package} grams.')
+else:
+    peso_pqt /= 1000
+    print(f'The total weight of the package is {package} kilos.')
