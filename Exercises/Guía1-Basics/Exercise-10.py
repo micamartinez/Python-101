1a936210c415cf6e22188a118f6d80cf7dfd14a5
Micaela <92948781+micamartinez@users.noreply.github.com>
2021-11-02 23:10:27 -0300

    Delete Exercise-10.py

--- Exercises/Guía1-Basics/Exercise-10.py
+++
@@ -1,9 +0,0 @@
-'''Create a program that asks the user for their age and shows on the screen if
-is of legal age or not, being 18 years of legal age.'''
-
-age = int(input('How old are you? '))
-
-if age >= 18:
-	print('The user is of legal age')
-else:
-	print('The user is a minor')
