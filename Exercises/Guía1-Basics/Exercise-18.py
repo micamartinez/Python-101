2614447900caf5571128b7d38d37070ccc41fd4c
Micaela <92948781+micamartinez@users.noreply.github.com>
2021-11-02 23:11:29 -0300

    Delete Exercise-18.py

--- Exercises/Guía1-Basics/Exercise-18.py
+++
@@ -1,7 +0,0 @@
-''' Create a program that asks the user for a word and then displays 
-the letters of the word entered one by one, starting with the last one.'''
-
-word = input('Enter a word: ')
-
-for letter in reversed(word):
-    print(letter)
