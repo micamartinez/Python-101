25675e95a1e55f56c0df717c1791afa5b22c5889
Micaela <92948781+micamartinez@users.noreply.github.com>
2021-11-02 23:11:46 -0300

    Delete Exercise-20.py

--- Exercises/Guía1-Basics/Exercise-20.py
+++
@@ -1,9 +0,0 @@
-'''Create a program that will echo everything the user enters 
-until the user types "exit" which will finish.'''
-
-while True:
-    word = input('Write a word or write "exit" to finish: ')
-    if word != 'exit':
-        print(word)
-    else:
-        break
