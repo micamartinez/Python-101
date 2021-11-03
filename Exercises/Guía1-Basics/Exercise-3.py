cd9dc9874e33ac4323b955df87806500cccc365d
Micaela <92948781+micamartinez@users.noreply.github.com>
2021-11-02 23:11:52 -0300

    Delete Exercise-3.py

--- Exercises/Guía1-Basics/Exercise-3.py
+++
@@ -1,8 +0,0 @@
-'''Create a program that asks the user for their name and age 
-and prints these data in different lines.'''
-
-name = input('Name: ')
-age = input('Age: ')
-
-print(name)
-print(age)
