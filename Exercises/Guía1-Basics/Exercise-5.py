58b26bed414228d602753bfd0640c4324df9597c
Micaela <92948781+micamartinez@users.noreply.github.com>
2021-11-02 23:12:03 -0300

    Delete Exercise-5.py

--- Exercises/Guía1-Basics/Exercise-5.py
+++
@@ -1,7 +0,0 @@
-'''Create a program that asks for the user's name and after the 
-user enters it, it shows on the screen <NAME> has <n> letters.'''
-
-NAME = input('Enter your name: ')
-n = len(NAME) 
-
-print(f'{NAME} has {n} letters.')
