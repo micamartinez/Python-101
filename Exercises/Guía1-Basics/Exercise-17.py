b22d38c7d7c433cc4fb9345e1d31ee5b16567b45
Micaela <92948781+micamartinez@users.noreply.github.com>
2021-11-02 23:11:22 -0300

    Delete Exercise-17.py

--- Exercises/Guía1-Basics/Exercise-17.py
+++
@@ -1,7 +0,0 @@
-# Create a program that displays the multiplication table from 1 to 10 on the screen.
-
-for i in range(1,11):
-    print(f'Multiplication table of {i}')
-    for j in range(10):
-        table = i * j
-        print(f'{i} * {j} = {table}')
